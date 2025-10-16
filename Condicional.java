df8e568b7e1be26f9565522c576357c7ba78baa8
Rodrigo <meuemail.rodrigodev@gmail.com>
2025-10-16 20:15:13 -0300

    Condicional if e else

---
+++ Condicional.java
@@ -0,0 +1,13 @@
+
+public class Condicional {
+    public static void main(String[] args) {
+        int idade = 17;
+        String nome = "João";
+
+        if (idade >= 18) {
+            System.out.println(nome + " é maior de idade.");
+        } else {
+            System.out.println(nome + " é menor de idade.");
+        }
+    }
+}
