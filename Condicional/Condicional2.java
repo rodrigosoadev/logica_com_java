383955b5ccf2a82595b30d53f67f063df018cd49
Rodrigo <meuemail.rodrigodev@gmail.com>
2025-10-17 19:48:05 -0300

    Condicionais

---
+++ Condicional/Condicional2.java
@@ -0,0 +1,17 @@
+package Condicional;
+
+public class Condicional2 {
+    public static void main(String[] args) {
+        float salario = 1899.99f;
+        float resultado = 0f;
+
+
+        if (salario > 4500) {
+            resultado = salario * 0.3f;
+            System.out.println("A porcentagem é 30% e o resultado é "+ resultado);
+        }else {
+            resultado = salario * 0.1f;
+            System.out.println("A porcentagem é 10% e o resultado é "+resultado);
+    }
+}
+}
---
+++ Condicional/ImpaPar.java
@@ -0,0 +1,13 @@
+package Condicional;
+
+public class ImpaPar {
+    public static void main(String[] args) {
+        int numero = 2;
+
+        if (numero % 2 == 0) {
+            System.out.println("O número " + numero + " é par");
+        }else {
+            System.out.println("O número " + numero + " é ímpar");
+        }
+    }
+}
