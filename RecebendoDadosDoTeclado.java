71a8f2ed85ed70b0c8768f3c44834bcabe4ca351
Rodrigo <meuemail.rodrigodev@gmail.com>
2025-10-21 19:53:34 -0300

    Input do teclado

---
+++ RecebendoDadosDoTeclado.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class RecebendoDadosDoTeclado {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o seu nome: ");
+        String nome = teclado.next();
+        System.out.println("Olá " + nome);
+    }
+}
