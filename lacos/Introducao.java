d3835dbac8f7d20f50487b91647a5dd257628d6f
Rodrigo <meuemail.rodrigodev@gmail.com>
2025-10-29 20:22:04 -0300

    laços

---
+++ lacos/Introducao.java
@@ -0,0 +1,12 @@
+package lacos;
+
+public class Introducao {
+    public static void main(String[] args) {
+        int contador = 0;
+
+        while (contador <= 10){
+            System.out.println("Contador: "+ contador);
+            contador = contador + 1;
+        }
+    }
+}
