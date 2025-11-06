30f1b67506aa2cc2d5e9da3eb819b1d7f1bc9c13
Rodrigo <meuemail.rodrigodev@gmail.com>
2025-11-06 20:57:56 -0300

    correção array

--- arrays/exercicioArrays.java
+++ arrays/exercicioArrays.java
@@ -18,7 +18,12 @@ public class exercicioArrays {
             System.out.println("Vetor 2, posição " + i + ": ");
             array2[i] = scanner.nextInt();
         }
-
-
+        System.out.println("Vetor 3 multiplicado");
+        for (int i = 0; i < array2.length; i++) {
+            System.out.println("Vetor 2, posição " + i + ": ");
+            array3[i] =  array1[i] * array2[i];
+            System.out.print(array3[i]+  " ");
+        }
+        
     }
 }
