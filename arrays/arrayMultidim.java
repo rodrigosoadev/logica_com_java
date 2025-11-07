008ee8abec9413e719050b7ca2d31e63c42fa2d7
Rodrigo <meuemail.rodrigodev@gmail.com>
2025-11-06 21:24:25 -0300

    array multidimensionais

---
+++ arrays/arrayMultidim.java
@@ -0,0 +1,13 @@
+package arrays;
+
+//array multidimensionais
+public class arrayMultidim {
+    public static void main(String[] args) {
+        int[] [] arrayMult = new int[2][2];
+        arrayMult[0][0] =22 ;
+        arrayMult[0][1] = 11;
+        arrayMult[1][0] = 44;
+        arrayMult[1][1] = 55;
+        System.out.println(arrayMult[1][0]);
+    }
+}
